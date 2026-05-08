4ab6ca1250e0c41950461fd92bad8dc63cf3351c
karthikcsop3-gif <karthikcsop3@gmail.com>
2026-05-09 00:20:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("message.html")
+    return render_template("interactive_message.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
